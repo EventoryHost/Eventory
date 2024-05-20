3537e157aa7d7178882ba0af74475ea65c63cb7d
meanmachine889 <ybharadwaj131@gmail.com>
2024-05-20 14:08:08 +0530

    made a landing page

---
+++ src/components/HeroComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import '../../src/App.css'
+
+function HeroComponent() {
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200 main m-0 w-screen">
+        <div className="hero-overlay opacity-90"></div>
+        <div className="hero-content flex-col lg:flex-row w-screen">
+          <div className="w-screen p-2">
+            <h1 className="text-[6rem] font-bold gradient-text text-white">Eventory</h1>
+            <p className="py-3 text-white text-2xl font-semibold pl-1">
+              Get Ready for the Extraordinary: <span className="">Site Launching Soon!</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HeroComponent;
