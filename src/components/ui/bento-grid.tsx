2d55d374d19e1059105da1072c45820db48c060b
meanmachine889 <ybharadwaj131@gmail.com>
2024-06-09 13:28:42 +0530

    made footer and responsive

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -11,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[15rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto",
+        "grid md:auto-rows-[13rem] auto-rows-[10rem] grid-cols-3 md:grid-cols-4 md:gap-4 gap-1 max-w-5xl mx-auto",
         className
       )}
     >
@@ -32,7 +32,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "relative row-span-1 rounded-xl h-[15rem] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none dark:bg-black dark:border-white/[0.2] bg-white border border-transparent flex flex-col overflow-hidden",
+        "relative row-span-1 rounded-xl md:min-h-[13rem] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none dark:bg-black dark:border-white/[0.2] bg-white border border-transparent flex flex-col overflow-hidden",
         className
       )}
     >
@@ -42,7 +42,7 @@ export const BentoGridItem = ({
         className="absolute inset-0 w-full h-full object-cover"
       />
       <div className="relative z-10 flex flex-col justify-end h-full p-4 bg-black bg-opacity-50">
-        <div className="font-poppins font-semibold text-2xl text-white mt-2">
+        <div className="font-poppins md:font-md md:text-2xl text-sm text-white mt-2">
           {title}
         </div>
       </div>
