bb66d4e0b8e089eebcbc8ea9f25bbba5b8c6ba94
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-06-13 22:48:56 +0530

    Update media queries - search bar

--- src/app/(components)/nav.tsx
+++ src/app/(components)/nav.tsx
@@ -42,7 +42,7 @@ const Nav: React.FC = () => {
               Search
             </label>
             <div className="relative">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-5 pointer-events-none">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-2 xl:pl-5 pointer-events-none">
                 <svg
                   className="w-4 h-4 text-gray-500 dark:text-gray-400"
                   aria-hidden="true"
@@ -62,7 +62,7 @@ const Nav: React.FC = () => {
               <input
                 type="text"
                 id="default-search"
-                className="block w-full lg:w-[27rem] p-2 pl-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+                className="block w-full lg:w-[18rem] xl:w-[27rem] p-2 pl-7 xl:pl-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
                 placeholder="Search for vendors, occasions or location"
                 value={query}
                 onChange={async (e) => {
@@ -121,7 +121,7 @@ const Nav: React.FC = () => {
           className={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto`}
           id="navbar-dropdown"
         >
-          <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col items-center font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-4 xl:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {tabs.map(([tab, redirect], index) => (
               <li key={index}>
                 <a
