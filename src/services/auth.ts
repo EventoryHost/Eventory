af64670bcf91f43a852ff67253eadf1f576923d8
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-06-26 21:15:18 +0530

    fix base url for hitting backend

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 export const authWithGoogle = async () => {
   try {
-    await axios.get(`${process.env.BASE_URL}/auth/google-auth`);
+    await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/google-auth`);
   } catch (error) {
     if (axios.isAxiosError(error)) {
       throw Error(error.message);
