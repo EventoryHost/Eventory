67906e8790d8c69f01617a85b4a4f9fb2ff36344
QuarterColon <ankitchakraborty1972@gmail.com>
2024-06-27 17:31:45 +0530

    updated auth service with passwordless auth fncs

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -9,3 +9,62 @@ export const authWithGoogle = async () => {
     }
   }
 };
+
+export const signUp = async (mobile: String) => {
+  try {
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/signup`, {
+      mobile,
+    });
+    return res;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw Error(error.message);
+    }
+  }
+};
+
+export const verifySignUpOtp = async (mobile: String, otp: String) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/auth/verify-otp-signup`,
+      { mobile, otp },
+    );
+
+    return res;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw Error(error.message);
+    }
+  }
+};
+
+export const verifyLoginOtp = async (
+  mobile: String,
+  code: String,
+  session: String,
+) => {
+  try {
+    const res = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/auth/verify-otp-login`,
+      { mobile, code, session },
+    );
+    return res;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw Error(error.message);
+    }
+  }
+};
+
+export const login = async (mobile: String) => {
+  try {
+    const res = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/login`, {
+      mobile,
+    });
+    return res;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw Error(error.message);
+    }
+  }
+}
