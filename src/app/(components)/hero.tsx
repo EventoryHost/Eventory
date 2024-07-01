4e83f8966df98017fcbd93bcb6ad6076f26566bc
arnavjagia <arnavjagia@users.noreply.github.com>
2024-07-01 17:32:23 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(components)/hero.tsx
+++ src/app/(components)/hero.tsx
@@ -21,9 +21,9 @@ const Feed: React.FC = () => {
 
   return (
     <>
-      <div className="flex lpt:h-[90vh] overflow-hidden flex-wrap md:py-0  w-[100%] flex-col justify-start md:flex-row">
+      <div className="flex w-[100%] flex-col flex-wrap justify-start overflow-hidden md:flex-row md:py-0 lpt:h-[90vh]">
         <div className="flex w-[100%] items-center justify-start lg:w-[50%]">
-          <div className=" mt-[10%] flex h-[90vh] flex-col items-start justify-start px-[4%] lg:mb-[40%] mb-[10%]">
+          <div className="mb-[10%] mt-[10%] flex h-[90vh] flex-col items-start justify-start px-[4%] lg:mb-[40%]">
             <h1 className="text-4xl font-bold md:text-6xl">
               Find the Perfect
               <br />
@@ -33,15 +33,15 @@ const Feed: React.FC = () => {
               Discover, compare, and book top-rated vendors for weddings,
               corporate events, parties, and more – all in one place.
             </p>
-            <div className="mt-7 self-start flex justify-center items-start">
+            <div className="mt-7 flex items-start justify-center self-start">
               <DropdownBar />
             </div>
           </div>
         </div>
         <div className="ml-4 flex-1 pr-5">
-          <div className=" max-w-[100%]">
+          <div className="max-w-[100%]">
             {isMounted && isMediumScreenOrLarger && (
-              <ParallaxScroll className="custom-class " />
+              <ParallaxScroll className="custom-class" />
             )}
           </div>
         </div>
