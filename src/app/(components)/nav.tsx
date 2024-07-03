3482a566c5495d3a3ac9423c7408abbbf09dbfde
sid0000007 <sid0000007@users.noreply.github.com>
2024-07-03 18:08:19 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(components)/nav.tsx
+++ src/app/(components)/nav.tsx
@@ -23,7 +23,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-rgba(0,0,0,0.12) navbar flex flex-col shadow-md">
-      <div className="container flex justify-between items-center">
+      <div className="container flex items-center justify-between">
         {/* logo eventory part */}
         <div className="logo flex flex-col items-center justify-center">
           <Link href={"/"}>
