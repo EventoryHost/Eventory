c5997d921da7bb38810f47bef85bfd7a3ab2e472
sid0000007 <mr.siddharth9267963125@gmail.com>
2024-07-04 17:17:33 +0530

    hero section padding

--- src/app/(components)/hero.tsx
+++ src/app/(components)/hero.tsx
@@ -22,7 +22,7 @@ const Feed: React.FC = () => {
     <>
       <div className="flex w-[100%] flex-col flex-wrap justify-start overflow-hidden md:flex-row md:py-0 lpt:h-[90vh]">
         <div className="flex w-[100%] items-center justify-start lg:w-[50%]">
-          <div className="mb-[10%] mt-[10%] flex h-[90vh] flex-col items-start justify-start px-[4%] lg:mb-[40%]">
+          <div className="mb-[10%] mt-[10%] flex h-[70vh] md:h-[80vh]  lg:h-[90vh] flex-col items-start justify-start px-[4%] lg:mb-[40%]">
             <h1 className="text-4xl font-bold md:text-6xl">
               Find the Perfect
               <br />
