a851086b248adbdf3cb6e45d6f89e36609e4002f
sid0000007 <mr.siddharth9267963125@gmail.com>
2024-07-11 12:26:05 +0530

    error fixing

--- src/app/(vendor onboarding)/venueProvider/page4/(components)/page.tsx
+++ src/app/(vendor onboarding)/venueProvider/page4/(components)/page.tsx
@@ -1,36 +1,36 @@
-"use client";
-import React from "react";
-import { useState } from "react";
-import PriceSlider from "./priceSlider";
+// "use client";
+// import React from "react";
+// import { useState } from "react";
+// import PriceSlider from "./priceSlider";
 
-const page = () => {
-  const [value1, setValue1] = useState([10, 50]);
-  const [value2, setValue2] = useState([20, 70]);
-  const [value3, setValue3] = useState([30, 90]);
+// const page = () => {
+//   const [value1, setValue1] = useState([10, 50]);
+//   const [value2, setValue2] = useState([20, 70]);
+//   const [value3, setValue3] = useState([30, 90]);
 
-  const handleChange1 = (event: Event, newValue: number | number[]) => {
-    setValue1(newValue as number[]);
-    console.log("value1", value1);
-  };
+//   const handleChange1 = (event: Event, newValue: number | number[]) => {
+//     setValue1(newValue as number[]);
+//     console.log("value1", value1);
+//   };
 
-  const handleChange2 = (event: Event, newValue: number | number[]) => {
-    setValue2(newValue as number[]);
-    console.log("value2", value2);
-  };
+//   const handleChange2 = (event: Event, newValue: number | number[]) => {
+//     setValue2(newValue as number[]);
+//     console.log("value2", value2);
+//   };
 
-  const handleChange3 = (event: Event, newValue: number | number[]) => {
-    setValue3(newValue as number[]);
-    console.log("value3", value3);
-  };
+//   const handleChange3 = (event: Event, newValue: number | number[]) => {
+//     setValue3(newValue as number[]);
+//     console.log("value3", value3);
+//   };
 
-  return (
-    <div className="flex w-[90vw] justify-center">
-      <h1>Price Sliders</h1>
-      <PriceSlider value={value1} onChange={handleChange1} />
-      <PriceSlider value={value2} onChange={handleChange2} />
-      <PriceSlider value={value3} onChange={handleChange3} />
-    </div>
-  );
-};
+//   return (
+//     <div className="flex w-[90vw] justify-center">
+//       <h1>Price Sliders</h1>
+//       <PriceSlider value={value1} onChange={handleChange1} />
+//       <PriceSlider value={value2} onChange={handleChange2} />
+//       <PriceSlider value={value3} onChange={handleChange3} />
+//     </div>
+//   );
+// };
 
-export default page;
+// export default page;
