ca9737bf13784b3a359f6f395aa566182b541af8
sumit-1803 <sumit-1803@users.noreply.github.com>
2024-07-30 17:18:13 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(components)/stepBar.tsx
+++ src/app/(components)/stepBar.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const StepBar = ({ currentStep }: { currentStep: number }) => {
-    return (
-        <div className="flex h-[50%] flex-col items-start justify-center gap-3 px-9 mt-9 xs:pl-5 md:px-11 lg:p-8 ">
-            <p className="text-3xl text-gray-900 font-medium mr-2">Step {currentStep} of 4</p>
-            <div className="flex">
-                <div
-                    className={`w-14 h-2 rounded-full ${currentStep >= 1 ? 'bg-[#252775]' : 'bg-gray-300'} opacity-100`}
-                />
-                <div
-                    className={`w-14 h-2 rounded-full ml-2 ${currentStep >= 2 ? 'bg-[#252775]' : 'bg-gray-300'} opacity-100`}
-                />
-                <div
-                    className={`w-14 h-2 rounded-full ml-2 ${currentStep >= 3 ? 'bg-[#252775]' : 'bg-gray-300'} opacity-100`}
-                />
-                <div
-                    className={`w-14 h-2 rounded-full ml-2 ${currentStep >= 4 ? 'bg-[#252775]' : 'bg-gray-300'} opacity-100`}
-                />
-            </div>
-        </div>
-    );
+  return (
+    <div className="mt-9 flex h-[50%] flex-col items-start justify-center gap-3 px-9 xs:pl-5 md:px-11 lg:p-8">
+      <p className="mr-2 text-3xl font-medium text-gray-900">
+        Step {currentStep} of 4
+      </p>
+      <div className="flex">
+        <div
+          className={`h-2 w-14 rounded-full ${currentStep >= 1 ? "bg-[#252775]" : "bg-gray-300"} opacity-100`}
+        />
+        <div
+          className={`ml-2 h-2 w-14 rounded-full ${currentStep >= 2 ? "bg-[#252775]" : "bg-gray-300"} opacity-100`}
+        />
+        <div
+          className={`ml-2 h-2 w-14 rounded-full ${currentStep >= 3 ? "bg-[#252775]" : "bg-gray-300"} opacity-100`}
+        />
+        <div
+          className={`ml-2 h-2 w-14 rounded-full ${currentStep >= 4 ? "bg-[#252775]" : "bg-gray-300"} opacity-100`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default StepBar;
