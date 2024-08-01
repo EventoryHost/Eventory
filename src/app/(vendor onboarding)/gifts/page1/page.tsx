99c8dbddcec682cfcee64c59ff4708a0ea6de0e4
sumit-1803 <sumit-1803@users.noreply.github.com>
2024-08-01 07:19:55 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(vendor onboarding)/gifts/page1/page.tsx
+++ src/app/(vendor onboarding)/gifts/page1/page.tsx
@@ -16,7 +16,6 @@ const workerOptions = [
 const Page = () => {
   const router = useRouter();
 
-
   return (
     <div className="flex h-full min-h-[calc(100vh-5.2rem)] w-full flex-col overflow-hidden lg:flex-row">
       <div className="flex flex-col items-start justify-between bg-[#FFFFFF] xs:gap-7 xs:pt-4 md:min-w-[30%] lg:max-w-[30%]">
@@ -41,14 +40,14 @@ const Page = () => {
       </div>
       <div className="flex w-full flex-col items-center justify-center bg-[#F7F6F9] p-2 md:p-[1rem]">
         <div className="flex flex-col gap-7 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
-          <span className="text-2xl font-semibold">
-            Basic Details
-          </span>
+          <span className="text-2xl font-semibold">Basic Details</span>
 
           <div className="flex">
             <div className="flex w-1/2 flex-col items-center justify-between gap-5 md:flex-row">
               <div className="flex w-full flex-col gap-4 md:w-[60%]">
-                <label className="font-medium" htmlFor="vendorName">Vendor Name (Manager)</label>
+                <label className="font-medium" htmlFor="vendorName">
+                  Vendor Name (Manager)
+                </label>
                 <input
                   id="vendorName"
                   type="text"
@@ -60,7 +59,9 @@ const Page = () => {
 
             <div className="flex w-1/2 flex-col items-center justify-between gap-5 md:flex-row">
               <div className="flex w-full flex-col gap-4 md:w-[60%]">
-                <label className="font-medium" htmlFor="contactNumber">Contact Number (POC)</label>
+                <label className="font-medium" htmlFor="contactNumber">
+                  Contact Number (POC)
+                </label>
                 <input
                   id="contactNumber"
                   type="text"
@@ -71,33 +72,39 @@ const Page = () => {
             </div>
           </div>
           <div className="container">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
               <div className="space-y-4">
                 <div>
-                  <label htmlFor="venueDescription" className="block mb-2 font-medium text-gray-900">
+                  <label
+                    htmlFor="venueDescription"
+                    className="mb-2 block font-medium text-gray-900"
+                  >
                     Your Description
                   </label>
                   <textarea
                     id="venueDescription"
                     placeholder="Enter your Venue description"
-                    className="shadow h-36 appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
+                    className="focus:shadow-outline h-36 w-full resize-none appearance-none rounded-xl border px-3 py-2 leading-tight text-gray-700 shadow focus:outline-none"
                   />
                 </div>
                 <div>
-                  <label htmlFor="minimumQuantity" className="block mb-2 font-medium text-gray-900">
+                  <label
+                    htmlFor="minimumQuantity"
+                    className="mb-2 block font-medium text-gray-900"
+                  >
                     What is the minimum quantity of order?
                   </label>
                   <input
                     type="text"
                     id="minimumQuantity"
                     placeholder="Enter minimum quantity"
-                    className="shadow appearance-none border rounded-xl w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    className="focus:shadow-outline w-full appearance-none rounded-xl border px-3 py-4 leading-tight text-gray-700 shadow focus:outline-none"
                   />
                 </div>
               </div>
               <div className="space-y-4">
                 <div>
-                  <label className="block mb-2 font-medium text-gray-900">
+                  <label className="mb-2 block font-medium text-gray-900">
                     Do you offer products in bulk quantity?
                   </label>
                   <div className="flex items-center">
@@ -117,7 +124,7 @@ const Page = () => {
                       id="noBulkQuantity"
                       type="radio"
                       name="bulkQuantity"
-                      className="form-radio h-4 w-4 text-indigo-600 focus:ring-indigo-500 ml-4"
+                      className="form-radio ml-4 h-4 w-4 text-indigo-600 focus:ring-indigo-500"
                     />
                     <label
                       htmlFor="noBulkQuantity"
@@ -128,7 +135,7 @@ const Page = () => {
                   </div>
                 </div>
                 <div>
-                  <label className="block mb-2 font-medium text-gray-900">
+                  <label className="mb-2 block font-medium text-gray-900">
                     Do you provide customizable gifts?
                   </label>
                   <div className="flex items-center">
@@ -148,7 +155,7 @@ const Page = () => {
                       id="noCustomizableGifts"
                       type="radio"
                       name="customizableGifts"
-                      className="form-radio h-4 w-4  text-indigo-600 focus:ring-indigo-500 ml-4"
+                      className="form-radio ml-4 h-4 w-4 text-indigo-600 focus:ring-indigo-500"
                     />
                     <label
                       htmlFor="noCustomizableGifts"
@@ -157,16 +164,16 @@ const Page = () => {
                       No
                     </label>
                   </div>
-                  <div className="items-strech mt-56 ml-20 flex flex-row gap-7 self-end">
+                  <div className="items-strech ml-20 mt-56 flex flex-row gap-7 self-end">
                     <button
                       className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                    // onClick={handleSubmit}
+                      // onClick={handleSubmit}
                     >
                       Skip
                     </button>
                     <button
                       className="rounded-xl bg-[rgb(46,49,146)] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                    // onClick={handleSubmit}
+                      // onClick={handleSubmit}
                     >
                       Continue
                     </button>
@@ -176,15 +183,7 @@ const Page = () => {
             </div>
           </div>
         </div>
-
-
-
-
-
-
       </div>
-
-
     </div>
   );
 };
