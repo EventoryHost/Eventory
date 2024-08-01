ef80113a5d889bd2994ce8c7b67cf402e4321195
Sumit Kumar <sumit182003@gmail.com>
2024-08-01 12:48:57 +0530

    added page one gifts

---
+++ src/app/(vendor onboarding)/gifts/page1/page.tsx
@@ -0,0 +1,192 @@
+"use client";
+import StepBar from "@/app/(components)/stepBar";
+import ThreeStepBar from "@/app/(components)/threeStepBar";
+import { Combobox } from "@/components/ui/combobox";
+import { Upload } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useRef, useState } from "react";
+
+const workerOptions = [
+  { value: "0-2 Members", label: "0-2 Members " },
+  { value: "2-4 Members", label: "2-4 Members" },
+  { value: "4-7 Members", label: "4-7 Members" },
+  { value: "More than 10 Members", label: "More than 10 Members" },
+];
+
+const Page = () => {
+  const router = useRouter();
+
+
+  return (
+    <div className="flex h-full min-h-[calc(100vh-5.2rem)] w-full flex-col overflow-hidden lg:flex-row">
+      <div className="flex flex-col items-start justify-between bg-[#FFFFFF] xs:gap-7 xs:pt-4 md:min-w-[30%] lg:max-w-[30%]">
+        <div className="flex items-center justify-start gap-1 xs:self-start xs:pl-5 lg:mt-[2rem]">
+          <ThreeStepBar currentStep={1} />
+        </div>
+        <div className="ml-8 flex h-[50%] flex-col items-start justify-center gap-9 px-9 xs:pl-5 md:px-11 lg:p-8">
+          <h1 className="text-2xl font-semibold md:text-4xl lg:text-5xl">
+            Fill out your Basic details
+          </h1>
+          <p className="text-gray-600 xs:text-sm md:w-[90%] lg:text-lg">
+            Please provide the details of the business offered by your company.
+          </p>
+        </div>
+        <div className="relative h-[10rem] lg:w-full">
+          <img
+            src={"/tajmahal.png"}
+            alt="Taj Mahal"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+      <div className="flex w-full flex-col items-center justify-center bg-[#F7F6F9] p-2 md:p-[1rem]">
+        <div className="flex flex-col gap-7 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
+          <span className="text-2xl font-semibold">
+            Basic Details
+          </span>
+
+          <div className="flex">
+            <div className="flex w-1/2 flex-col items-center justify-between gap-5 md:flex-row">
+              <div className="flex w-full flex-col gap-4 md:w-[60%]">
+                <label className="font-medium" htmlFor="vendorName">Vendor Name (Manager)</label>
+                <input
+                  id="vendorName"
+                  type="text"
+                  className="w-full rounded-xl border-2 bg-white p-5 py-3 outline-none"
+                  placeholder="Enter your full name"
+                />
+              </div>
+            </div>
+
+            <div className="flex w-1/2 flex-col items-center justify-between gap-5 md:flex-row">
+              <div className="flex w-full flex-col gap-4 md:w-[60%]">
+                <label className="font-medium" htmlFor="contactNumber">Contact Number (POC)</label>
+                <input
+                  id="contactNumber"
+                  type="text"
+                  className="w-full rounded-xl border-2 bg-white p-5 py-3 outline-none"
+                  placeholder="Enter your contact number"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="space-y-4">
+                <div>
+                  <label htmlFor="venueDescription" className="block mb-2 font-medium text-gray-900">
+                    Your Description
+                  </label>
+                  <textarea
+                    id="venueDescription"
+                    placeholder="Enter your Venue description"
+                    className="shadow h-36 appearance-none border rounded-xl w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline resize-none"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="minimumQuantity" className="block mb-2 font-medium text-gray-900">
+                    What is the minimum quantity of order?
+                  </label>
+                  <input
+                    type="text"
+                    id="minimumQuantity"
+                    placeholder="Enter minimum quantity"
+                    className="shadow appearance-none border rounded-xl w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  />
+                </div>
+              </div>
+              <div className="space-y-4">
+                <div>
+                  <label className="block mb-2 font-medium text-gray-900">
+                    Do you offer products in bulk quantity?
+                  </label>
+                  <div className="flex items-center">
+                    <input
+                      id="bulkQuantity"
+                      type="radio"
+                      name="bulkQuantity"
+                      className="form-radio h-4 w-4 text-indigo-600 focus:ring-indigo-500"
+                    />
+                    <label
+                      htmlFor="bulkQuantity"
+                      className="ml-2 block text-sm font-medium text-gray-900"
+                    >
+                      Yes
+                    </label>
+                    <input
+                      id="noBulkQuantity"
+                      type="radio"
+                      name="bulkQuantity"
+                      className="form-radio h-4 w-4 text-indigo-600 focus:ring-indigo-500 ml-4"
+                    />
+                    <label
+                      htmlFor="noBulkQuantity"
+                      className="ml-2 block text-sm font-medium text-gray-900"
+                    >
+                      No
+                    </label>
+                  </div>
+                </div>
+                <div>
+                  <label className="block mb-2 font-medium text-gray-900">
+                    Do you provide customizable gifts?
+                  </label>
+                  <div className="flex items-center">
+                    <input
+                      id="customizableGifts"
+                      type="radio"
+                      name="customizableGifts"
+                      className="form-radio h-4 w-4 text-indigo-600 focus:ring-indigo-500"
+                    />
+                    <label
+                      htmlFor="customizableGifts"
+                      className="ml-2 block text-sm font-medium text-gray-900"
+                    >
+                      Yes
+                    </label>
+                    <input
+                      id="noCustomizableGifts"
+                      type="radio"
+                      name="customizableGifts"
+                      className="form-radio h-4 w-4  text-indigo-600 focus:ring-indigo-500 ml-4"
+                    />
+                    <label
+                      htmlFor="noCustomizableGifts"
+                      className="ml-2 block text-sm font-medium text-gray-900"
+                    >
+                      No
+                    </label>
+                  </div>
+                  <div className="items-strech mt-56 ml-20 flex flex-row gap-7 self-end">
+                    <button
+                      className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
+                    // onClick={handleSubmit}
+                    >
+                      Skip
+                    </button>
+                    <button
+                      className="rounded-xl bg-[rgb(46,49,146)] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
+                    // onClick={handleSubmit}
+                    >
+                      Continue
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+
+      </div>
+
+
+    </div>
+  );
+};
+
+export default Page;
