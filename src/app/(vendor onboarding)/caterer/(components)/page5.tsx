5836692678e815badf395788f037d49298c2817d
meanmachine889 <meanmachine889@users.noreply.github.com>
2024-08-01 16:11:21 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(vendor onboarding)/caterer/(components)/page5.tsx
+++ src/app/(vendor onboarding)/caterer/(components)/page5.tsx
@@ -534,14 +534,10 @@ const Page5: React.FC<PageProps> = ({
                 </button>
               </div>
               <div className="items-strech mt-9 flex flex-row gap-7 self-end">
-                <button
-                  className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                >
+                <button className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3">
                   Skip
                 </button>
-                <button
-                  className="rounded-xl bg-[#2E3192] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                >
+                <button className="rounded-xl bg-[#2E3192] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3">
                   Continue
                 </button>
               </div>
