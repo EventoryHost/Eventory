2d24ef6752790693d217fad77c144103003a33e9
arnavjagia <arnavjagia@users.noreply.github.com>
2024-08-03 17:33:39 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(vendor onboarding)/gifts/page1/page.tsx
+++ src/app/(vendor onboarding)/gifts/page1/page.tsx
@@ -34,7 +34,6 @@ const Page1: React.FC<PageProps> = ({
   handleChange,
   handleNestedChange,
 }) => {
-
   return (
     <div className="flex h-full min-h-[calc(100vh-5.2rem)] w-full flex-col overflow-hidden lg:flex-row">
       <div className="flex flex-col items-start justify-between bg-[#FFFFFF] xs:gap-7 xs:pt-4 md:min-w-[30%] lg:max-w-[30%]">
