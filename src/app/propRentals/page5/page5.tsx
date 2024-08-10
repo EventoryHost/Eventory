a3a22b8c9eaeeb676391c2a0ad404fa096c22738
Sumit Kumar <sumit182003@gmail.com>
2024-08-10 17:35:20 +0530

    added page 1 and skeleton for prop rental

---
+++ src/app/propRentals/page5/page5.tsx
@@ -0,0 +1,30 @@
+import StepBar from "@/app/(components)/stepBar";
+
+function page5() {
+    return (
+      <div className="flex h-full min-h-[calc(100vh-5.2rem)] w-full flex-col overflow-hidden lg:flex-row">
+      <div className="flex flex-col items-start justify-between bg-[#FFFFFF] xs:gap-7 xs:pt-4 md:min-w-[30%] lg:max-w-[30%]">
+        <div className="flex items-center justify-start gap-1 xs:self-start xs:pl-5 lg:mt-[2rem]">
+          <StepBar currentStep={5} />
+        </div>
+        <div className="ml-8 flex h-[50%] flex-col items-start justify-center gap-9 px-9 xs:pl-5 md:px-11 lg:p-8">
+          <h1 className="text-2xl font-semibold md:text-4xl lg:text-5xl">
+            Fill out your Basic details
+          </h1>
+          <p className="text-gray-600 xs:text-sm md:w-[90%] lg:text-lg">
+            Please provide the details of the business offered by your company.
+          </p>
+        </div>
+        <div className="relative h-[10rem] lg:w-full">
+          <img
+            src={"/tajmahal.png"}
+            alt="Taj Mahal"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+    );
+  }
+  
+  export default page5;
