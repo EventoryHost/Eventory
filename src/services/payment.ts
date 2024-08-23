89cdce8e35520978b316f686a41bdd41f4ca0f14
QuarterColon <ankitchakraborty1972@gmail.com>
2024-08-23 12:02:46 +0530

    payment service added

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Razorpay from "razorpay";
 
 const createOrder = async (amount: string, plan: string, id: string) => {
   try {
@@ -7,7 +8,7 @@ const createOrder = async (amount: string, plan: string, id: string) => {
       {
         amount: amount,
         currency: "INR",
-        receipt: `payement of ${amount} INR for ${plan} plan by ${id}`,
+        receipt: `payement-${id}-${plan}-${amount}`,
       },
       {
         headers: {
@@ -24,4 +25,85 @@ const createOrder = async (amount: string, plan: string, id: string) => {
   }
 };
 
-export { createOrder };
+const verifyPayment = async (
+  order_id: string,
+  payment_id: string,
+  signature: string,
+) => {
+  try {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/payment/verify-payment/`,
+      {
+        order_id,
+        payment_id,
+        signature,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log(error.message);
+    }
+  }
+};
+
+const handlePayment = async (
+  amount: string,
+  plan: string,
+  id: string,
+  name: string,
+) => {
+  try {
+    const orderResponse = await createOrder(amount, plan, id);
+
+    const { id: order_id } = orderResponse;
+
+    const options = {
+      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY as string,
+      image:
+        "https://eventory-bucket.s3.ap-south-1.amazonaws.com/website/Logo.png",
+      amount: parseFloat(amount) * 100,
+      currency: "INR",
+      name: "Eventory",
+      description: `Payment of ${amount} INR for ${plan} plan by ${id}`,
+      handler: async function (response: any) {
+        const payment_id = response.razorpay_payment_id;
+        const order_id = response.razorpay_order_id;
+        const signature = response.razorpay_signature;
+
+        const verifyResponse = await verifyPayment(
+          order_id,
+          payment_id,
+          signature,
+        );
+
+        if (verifyResponse.status === "success") {
+          alert("Payment successful");
+        } else {
+          alert("Payment failed");
+        }
+      },
+      order_id: order_id,
+      prefill: {
+        name,
+      },
+      theme: {
+        color: "#2E3192",
+      },
+      
+    };
+
+    let rzp = new (window as any).Razorpay(options);
+    rzp.open();
+  } catch (error) {
+    console.error("Payment error:", error);
+  }
+};
+
+export { createOrder, handlePayment };
