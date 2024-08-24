dcb870f645e1da4bdb75e38a7fd331ee9d718236
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-08-24 12:07:06 +0530

    Refactor: venue-provider left page

--- src/app/(vendor onboarding)/venue-provider/page1/page1.tsx
+++ src/app/(vendor onboarding)/venue-provider/page1/page1.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import React, { useState } from "react";
-import Image from "next/image";
-
-import tajmahal from "/public/tajmahal.png";
-import { ComboboxDemo } from "@/components/dropdown";
-import { Dropdown } from "react-day-picker";
 import { Combobox } from "@/components/ui/combobox";
 
 const venueTypes = [
@@ -97,167 +91,117 @@ const Page1: React.FC<Page1Props> = ({ formState, updateFormState }) => {
   };
 
   return (
-    <div className="flex h-full min-h-[calc(100vh-5.2rem)] w-full flex-col overflow-hidden lg:flex-row">
-      <div className="flex flex-col items-start justify-between bg-[#FFFFFF] xs:gap-7 xs:pt-4 md:min-w-[30%] lg:max-w-[30%]">
-        <div className="flex items-center justify-start gap-1 xs:self-start xs:pl-5 md:px-11 lg:mt-[2rem]">
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-[#2E3192] p-5 text-white">
-            1
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            2
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            3
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
+    <div className="flex flex-col items-start gap-7 overflow-y-scroll rounded-xl bg-white p-3 xs:w-[100%] xs:min-w-[90%] xs:justify-start md:p-6">
+      <h1 className="text-3xl font-semibold">Basic Details</h1>
+      <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+        <div className="flex min-w-[40%] flex-col gap-4">
+          <label htmlFor="businessName">Venue Name</label>
+          <input
+            id="businessName"
+            type="text"
+            className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
+            placeholder="Enter your Venue Name"
+            value={venueName}
+            onChange={(e) => updateFormState({ venueName: e.target.value })}
+          />
         </div>
-        <div className="flex items-center justify-start gap-1 xs:self-start xs:pl-5 md:px-11 lg:mt-[1rem]">
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            4
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            5
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            6
-          </button>
-        </div>
-        <div className="flex h-[50%] flex-col items-start justify-center gap-9 px-9 xs:pl-5 md:px-11 lg:p-8">
-          <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">
-            Fill out your venue details
-          </h1>
-          <p className="text-black xs:text-sm md:w-[90%]">
-            Please provide the details of the venue offered by your company.
-          </p>
-        </div>
-        <div className="relative h-[10rem] lg:w-full">
-          <Image
-            src={tajmahal}
-            alt="Image of Indian monuments"
-            className="h-full w-full object-cover"
+        <div className="flex min-w-[40%] flex-col gap-4">
+          <label htmlFor="businessName">Venue Address</label>
+          <input
+            id="businessName"
+            type="text"
+            className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
+            placeholder="Enter your Venue address"
+            value={VenueAddress}
+            onChange={(e) => updateFormState({ VenueAddress: e.target.value })}
           />
         </div>
       </div>
-      <div className="flex min-w-[70%] flex-col items-center justify-center bg-[#F7F6F9] p-2 md:p-[1rem]">
-        <div className="flex flex-col gap-7 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
-          <h1 className="text-3xl font-semibold">Basic Details</h1>
-          <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-            <div className="flex min-w-[40%] flex-col gap-4">
-              <label htmlFor="businessName">Venue Name</label>
-              <input
-                id="businessName"
-                type="text"
-                className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
-                placeholder="Enter your Venue Name"
-                value={venueName}
-                onChange={(e) => updateFormState({ venueName: e.target.value })}
-              />
-            </div>
-            <div className="flex min-w-[40%] flex-col gap-4">
-              <label htmlFor="businessName">Venue Address</label>
-              <input
-                id="businessName"
-                type="text"
-                className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
-                placeholder="Enter your Venue address"
-                value={VenueAddress}
-                onChange={(e) =>
-                  updateFormState({ VenueAddress: e.target.value })
-                }
-              />
-            </div>
-          </div>
 
-          <div className="flex min-h-full min-w-full flex-col items-center gap-5">
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="category">Venue Type</label>
-                <Combobox
-                  options={venueTypes}
-                  placeholder="Select your category"
-                  setFunction={(value) => updateFormState({ venueType: value })}
-                  className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
-                />
-              </div>
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="category">Seating Capacity</label>
-                <Combobox
-                  options={seatingOptions}
-                  placeholder="Select your category"
-                  setFunction={(value) =>
-                    updateFormState({ seatingCapacity: value })
-                  }
-                  className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
-                />
-              </div>
-            </div>
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="category">Operating hours</label>
-                <Combobox
-                  options={timeOptions}
-                  placeholder="start timings..."
-                  setFunction={(value) =>
-                    updateFormState({ startOperatingHours: value })
-                  }
-                  className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
-                />
-                <Combobox
-                  options={timeOptions}
-                  placeholder="ending timings..."
-                  setFunction={(value) =>
-                    updateFormState({ endOperatingHours: value })
-                  }
-                  className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
-                />
-              </div>
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="category">Standing Capacity</label>
-                <Combobox
-                  options={standingOptions}
-                  placeholder="Select your category"
-                  setFunction={(value) =>
-                    updateFormState({ standingCapacity: value })
-                  }
-                  className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
-                />
-              </div>
-            </div>
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="businessName">Venue Description</label>
-                <input
-                  id="businessName"
-                  type="text"
-                  className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
-                  placeholder="Enter your Venue Description"
-                  value={venueDescription}
-                  onChange={(e) =>
-                    updateFormState({ venueDescription: e.target.value })
-                  }
-                />
-              </div>
-            </div>
-
-            <div className="items-strech mt-9 flex flex-row gap-7 self-end">
-              <button
-                className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                onClick={handleSubmit}
-              >
-                Skip
-              </button>
-              <button
-                className="rounded-xl bg-[#2E3192] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                onClick={handleSubmit}
-              >
-                Continue
-              </button>
-            </div>
+      <div className="flex min-h-full min-w-full flex-col items-center gap-5">
+        <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+          <div className="flex min-w-[40%] flex-col gap-4">
+            <label htmlFor="category">Venue Type</label>
+            <Combobox
+              options={venueTypes}
+              placeholder="Select your category"
+              setFunction={(value) => updateFormState({ venueType: value })}
+              className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
+            />
           </div>
+          <div className="flex min-w-[40%] flex-col gap-4">
+            <label htmlFor="category">Seating Capacity</label>
+            <Combobox
+              options={seatingOptions}
+              placeholder="Select your category"
+              setFunction={(value) =>
+                updateFormState({ seatingCapacity: value })
+              }
+              className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
+            />
+          </div>
+        </div>
+        <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+          <div className="flex min-w-[40%] flex-col gap-4">
+            <label htmlFor="category">Operating hours</label>
+            <Combobox
+              options={timeOptions}
+              placeholder="start timings..."
+              setFunction={(value) =>
+                updateFormState({ startOperatingHours: value })
+              }
+              className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
+            />
+            <Combobox
+              options={timeOptions}
+              placeholder="ending timings..."
+              setFunction={(value) =>
+                updateFormState({ endOperatingHours: value })
+              }
+              className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
+            />
+          </div>
+          <div className="flex min-w-[40%] flex-col gap-4">
+            <label htmlFor="category">Standing Capacity</label>
+            <Combobox
+              options={standingOptions}
+              placeholder="Select your category"
+              setFunction={(value) =>
+                updateFormState({ standingCapacity: value })
+              }
+              className="flex items-center justify-between rounded-xl border-2 py-6 hover:text-[#2E3192]"
+            />
+          </div>
+        </div>
+        <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+          <div className="flex min-w-[40%] flex-col gap-4">
+            <label htmlFor="businessName">Venue Description</label>
+            <input
+              id="businessName"
+              type="text"
+              className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
+              placeholder="Enter your Venue Description"
+              value={venueDescription}
+              onChange={(e) =>
+                updateFormState({ venueDescription: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <div className="items-strech mt-9 flex flex-row gap-7 self-end">
+          <button
+            className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
+            onClick={handleSubmit}
+          >
+            Skip
+          </button>
+          <button
+            className="rounded-xl bg-[#2E3192] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
+            onClick={handleSubmit}
+          >
+            Continue
+          </button>
         </div>
       </div>
     </div>
--- src/app/(vendor onboarding)/venue-provider/page5/page5.tsx
+++ src/app/(vendor onboarding)/venue-provider/page5/page5.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React from "react";
 import { Upload } from "lucide-react";
 import Appetizers from "../(components)/Appetizers";
 
@@ -57,155 +56,105 @@ const Page5: React.FC<Page5Props> = ({
   setVenue_special_features,
 }) => {
   return (
-    <div className="flex h-full min-h-[calc(100vh-5.2rem)] w-full flex-col overflow-hidden lg:flex-row">
-      <div className="flex flex-col items-start justify-between bg-[#FFFFFF] xs:gap-7 xs:pt-4 md:min-w-[30%] lg:max-w-[30%]">
-        <div className="flex items-center justify-start gap-1 xs:self-start xs:pl-5 md:px-11 lg:mt-[2rem]">
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-[#2E3192] p-5 text-white">
-            1
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            2
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            3
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-        </div>
-        <div className="flex items-center justify-start gap-1 xs:self-start xs:pl-5 md:px-11 lg:mt-[1rem]">
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            4
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            5
-          </button>
-          <div className="h-[0.3rem] w-[4rem] rounded-xl bg-gray-300"></div>
-          <button className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-300 p-5">
-            6
-          </button>
-        </div>
-        <div className="flex h-[50%] flex-col items-start justify-center gap-9 px-9 xs:pl-5 md:px-11 lg:p-8">
-          <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">
-            Fill out your venue details
-          </h1>
-          <p className="text-black xs:text-sm md:w-[90%]">
-            Please provide the details of the venue offered by your company.
-          </p>
-        </div>
-        <div className="relative h-[10rem] lg:w-full">
-          <img
-            src={"/tajmahal.png"}
-            alt=""
-            className="h-full w-full object-cover"
-          />
+    <div className="flex flex-col items-start gap-7 overflow-y-scroll rounded-xl bg-white p-3 xs:w-[100%] xs:min-w-[90%] xs:justify-start md:p-6">
+      <div className="flex flex-col gap-7 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
+        <h1 className="text-3xl font-semibold">Extra Details</h1>
+        <div className="flex min-h-full min-w-full flex-col items-center gap-5">
+          <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+            <div className="flex min-w-[40%] flex-col gap-4">
+              <div className="flex min-w-full flex-col items-start justify-between gap-2">
+                <p className="text-xl font-semibold">
+                  HD photos / Videos of your Venue
+                </p>
+                <p className="text-gray-500">PNG, PDF, JPG</p>
+                <button className="flex items-center justify-center gap-5 rounded-xl border-2 bg-gray-200 px-9 py-3 text-[#2E3192] hover:bg-[#2E3192] hover:text-white">
+                  {" "}
+                  <Upload />
+                  Upload
+                </button>
+              </div>
+            </div>
+            <div className="flex min-w-[40%] flex-col gap-4">
+              <div className="flex min-w-full flex-col items-start justify-between gap-2">
+                <p className="text-xl font-semibold">
+                  Virtual tour of your venue (Optional)
+                </p>
+                <p className="text-gray-500">PNG, PDF, JPG</p>
+                <button className="flex items-center justify-center gap-5 rounded-xl border-2 bg-gray-200 px-9 py-3 text-[#2E3192] hover:bg-[#2E3192] hover:text-white">
+                  {" "}
+                  <Upload />
+                  Upload
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+            <div className="flex min-w-[40%] flex-col gap-4">
+              <label htmlFor="category">Instagram URL (optional)</label>
+              <input
+                id="instaURL"
+                type="text"
+                className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
+                placeholder="Enter your instagram URL"
+                value={formState.instaURL}
+                onChange={(e) => updateFormState({ instaURL: e.target.value })}
+              />
+            </div>
+            <div className="flex min-w-[40%] flex-col gap-4">
+              <label htmlFor="category">Website URL (optional)</label>
+              <input
+                id="websiteURL"
+                type="text"
+                className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
+                placeholder="Enter your website URL"
+                value={formState.websiteURL}
+                onChange={(e) =>
+                  updateFormState({ websiteURL: e.target.value })
+                }
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <div className="scroll-touch flex max-h-[calc(100vh-5.2rem)] min-w-[70%] flex-col items-center gap-9 overflow-y-scroll bg-[#F7F6F9] p-2 md:p-[1rem]">
-        <div className="flex flex-col gap-7 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
-          <h1 className="text-3xl font-semibold">Extra Details</h1>
-          <div className="flex min-h-full min-w-full flex-col items-center gap-5">
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <div className="flex min-w-[40%] flex-col gap-4">
-                {/* <label htmlFor="category">Venue Type</label> */}
-                <div className="flex min-w-full flex-col items-start justify-between gap-2">
-                  <p className="text-xl font-semibold">
-                    HD photos / Videos of your Venue
-                  </p>
-                  <p className="text-gray-500">PNG, PDF, JPG</p>
-                  <button className="flex items-center justify-center gap-5 rounded-xl border-2 bg-gray-200 px-9 py-3 text-[#2E3192] hover:bg-[#2E3192] hover:text-white">
-                    {" "}
-                    <Upload />
-                    Upload
-                  </button>
-                </div>
-              </div>
-              <div className="flex min-w-[40%] flex-col gap-4">
-                {/* <label htmlFor="category">Seating Capacity</label> */}
-                <div className="flex min-w-full flex-col items-start justify-between gap-2">
-                  <p className="text-xl font-semibold">
-                    Virtual tour of your venue (Optional)
-                  </p>
-                  <p className="text-gray-500">PNG, PDF, JPG</p>
-                  <button className="flex items-center justify-center gap-5 rounded-xl border-2 bg-gray-200 px-9 py-3 text-[#2E3192] hover:bg-[#2E3192] hover:text-white">
-                    {" "}
-                    <Upload />
-                    Upload
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="category">Instagram URL (optional)</label>
-                <input
-                  id="instaURL"
-                  type="text"
-                  className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
-                  placeholder="Enter your instagram URL"
-                  value={formState.instaURL}
-                  onChange={(e) =>
-                    updateFormState({ instaURL: e.target.value })
-                  }
-                />
-              </div>
-              <div className="flex min-w-[40%] flex-col gap-4">
-                <label htmlFor="category">Website URL (optional)</label>
-                <input
-                  id="websiteURL"
-                  type="text"
-                  className="h-[4rem] w-full rounded-xl border-2 bg-white p-3 text-sm outline-none"
-                  placeholder="Enter your website URL"
-                  value={formState.websiteURL}
-                  onChange={(e) =>
-                    updateFormState({ websiteURL: e.target.value })
-                  }
-                />
-              </div>
-            </div>
+      <div className="flex flex-col gap-9 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
+        <h1 className="text-3xl font-semibold">
+          Restrictions and policies at your Venue{" "}
+        </h1>
+        <div className="flex min-h-full min-w-full flex-col items-center gap-5">
+          <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+            <Appetizers
+              appetizers={_venue_restrictions}
+              selectedAppetizers={venue_restrictions}
+              setSelectedAppetizers={setVenue_restrictions}
+            />
           </div>
         </div>
-        <div className="flex flex-col gap-9 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
-          <h1 className="text-3xl font-semibold">
-            Restrictions and policies at your Venue{" "}
-          </h1>
-          <div className="flex min-h-full min-w-full flex-col items-center gap-5">
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <Appetizers
-                appetizers={_venue_restrictions}
-                selectedAppetizers={venue_restrictions}
-                setSelectedAppetizers={setVenue_restrictions}
-              />
-            </div>
+      </div>
+      <div className="flex flex-col gap-9 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
+        <h1 className="text-3xl font-semibold">
+          Special Features at your Venue
+        </h1>
+        <div className="flex min-h-full min-w-full flex-col items-center gap-5">
+          <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
+            <Appetizers
+              appetizers={_venue_features}
+              selectedAppetizers={venue_special_features}
+              setSelectedAppetizers={setVenue_special_features}
+            />
           </div>
-        </div>
-        <div className="flex flex-col gap-9 rounded-xl bg-white p-3 xs:min-w-[90%] md:p-6">
-          <h1 className="text-3xl font-semibold">
-            Special Features at your Venue
-          </h1>
-          <div className="flex min-h-full min-w-full flex-col items-center gap-5">
-            <div className="flex min-w-full flex-col items-center justify-between gap-5 md:flex-row">
-              <Appetizers
-                appetizers={_venue_features}
-                selectedAppetizers={venue_special_features}
-                setSelectedAppetizers={setVenue_special_features}
-              />
-            </div>
-            <div className="items-strech mt-9 flex flex-row gap-7 self-end">
-              <button
-                className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                onClick={handleSubmit}
-              >
-                Skip
-              </button>
-              <button
-                className="rounded-xl bg-[#2E3192] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
-                onClick={handleSubmit}
-              >
-                Continue
-              </button>
-            </div>
+          <div className="items-strech mt-9 flex flex-row gap-7 self-end">
+            <button
+              className="rounded-xl border-2 border-[#2E3192] text-[#2E3192] xs:w-fit xs:px-3 xs:py-2 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
+              onClick={handleSubmit}
+            >
+              Skip
+            </button>
+            <button
+              className="rounded-xl bg-[#2E3192] text-white xs:w-fit xs:px-4 xs:py-3 md:w-fit md:min-w-[10rem] md:px-4 md:py-3"
+              onClick={handleSubmit}
+            >
+              Continue
+            </button>
           </div>
         </div>
       </div>
