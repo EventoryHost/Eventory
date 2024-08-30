88431667b6b72f5863abeef9f9fe6c5964891b73
meanmachine889 <meanmachine889@users.noreply.github.com>
2024-08-30 13:00:01 +0000

    chore: apply formatting changes [skip ci]

--- src/app/(vendor onboarding)/businessDetails/page.tsx
+++ src/app/(vendor onboarding)/businessDetails/page.tsx
@@ -18,11 +18,11 @@ const frameworks = [
 ];
 
 const categories = [
-  { value : 'pav', label: "PaV"},
-  { value : 'caterer' , label: "Caterers"},
-  { value : 'decorators', label: "Decorator"},
-  { value : 'propRentals', label: "Prop Rentals"},
-  { value : 'makeupArtist', label: "Makeup Artist"},
+  { value: "pav", label: "PaV" },
+  { value: "caterer", label: "Caterers" },
+  { value: "decorators", label: "Decorator" },
+  { value: "propRentals", label: "Prop Rentals" },
+  { value: "makeupArtist", label: "Makeup Artist" },
 ];
 
 const yearsInOperation = [
@@ -81,26 +81,26 @@ const BusinessDetails = () => {
 
     // Check if any required field is empty
     for (const key in refs.current) {
-        const refElement = refs.current[key as keyof businessDetails];
-        if (!refElement || !refElement.value.trim()) {
-            return; // Stop further processing if any required field is empty
-        }
+      const refElement = refs.current[key as keyof businessDetails];
+      if (!refElement || !refElement.value.trim()) {
+        return; // Stop further processing if any required field is empty
+      }
     }
 
     // Make sure the user has selected a category
     if (!businessDetails.category) {
-        return; // Stop further processing if category is not selected
+      return; // Stop further processing if category is not selected
     }
 
     const newDetails: businessDetails = {
-        businessName: refs.current.businessName!.value,
-        category: businessDetails.category,
-        gstin: refs.current.gstin!.value,
-        years: businessDetails.years,
-        businessAddress: refs.current.businessAddress!.value,
-        landmark: refs.current.landmark!.value,
-        pinCode: Number(refs.current.pinCode!.value),
-        cities: businessDetails.cities,
+      businessName: refs.current.businessName!.value,
+      category: businessDetails.category,
+      gstin: refs.current.gstin!.value,
+      years: businessDetails.years,
+      businessAddress: refs.current.businessAddress!.value,
+      landmark: refs.current.landmark!.value,
+      pinCode: Number(refs.current.pinCode!.value),
+      cities: businessDetails.cities,
     };
 
     setBusinessDetails(newDetails);
@@ -108,16 +108,15 @@ const BusinessDetails = () => {
 
     const token = localStorage.getItem("token")!;
     const { userId, email } = jwt.decode(token) as {
-        userId: string;
-        email: string;
+      userId: string;
+      email: string;
     };
 
     addBusinessDetails(userId, newDetails);
 
     // Redirect to the selected category's page
     Router.push(`/${businessDetails.category}`);
-};
-
+  };
 
   return (
     <div className="flex h-full min-h-screen w-full flex-col overflow-hidden lg:flex-row">
