cb88d9265fa965ed5f2b08645a08451914ab1180
Bhanupratapsingh21 <Bhanupratapsingh21@users.noreply.github.com>
2024-09-28 12:50:04 +0000

    chore: apply formatting changes [skip ci]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Footer from "./(components)/footer";
 import "./globals.css";
 import Nav from "./(components)/nav";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: "Eventory",
   description: "Inventory for Events",
